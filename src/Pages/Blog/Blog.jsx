0fba2dc2d000760b4214b6afb99d4732be6d5e92
Md Yeasin <mdyeasinhn@gmail.com>
2024-09-15 01:31:09 +0600

    published blog fetch to database

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,5 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+
 const Blog = () => {
-    
+    const axiosPublic = useAxiosPublic();
+    const { data: blogs = [], isLoading } = useQuery({
+        queryKey: ['blogs'],
+        queryFn: async () => {
+            const { data } = await axiosPublic.get("/all-blogs")
+            return data
+        }
+    })
+    console.log(blogs);
+
+    const publishedBlogs = blogs.filter(blog => blog.status === 'published');
 
     return (
         <div>
@@ -9,44 +22,41 @@ const Blog = () => {
                         <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl">Blood Donation Insights</h1>
 
                         <p className="max-w-lg mx-auto mt-4 text-gray-500">
-                        Stay Informed and Inspired with the Latest in Blood Donation News and Stories.
+                            Stay Informed and Inspired with the Latest in Blood Donation News and Stories.
                         </p>
                     </div>
 
-                    <div className="grid grid-cols-1 gap-8 mt-8 lg:grid-cols-2">
-                        <div>
-                            <img className="relative z-10 object-cover w-full rounded-md h-96" src="https://i.ibb.co/34vTSYH/240-F-133781652-kd-Km-M1-A3-Jz-Zoh-Ne-LN9b-Op-Y3b-M4liw-JGW.jpg" alt="" />
-
-                            <div className="relative z-20 max-w-lg p-6 mx-auto -mt-20 bg-white rounded-md shadow">
-                                <a href="#" className="font-semibold text-gray-800 hover:underline md:text-xl">
-                                    All the features you want to know
-                                </a>
-
-                                <p className="mt-3 text-sm text-gray-500 md:text-sm">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veritatis sint autem nesciunt,
-                                    laudantium quia tempore delect
-                                </p>
-
-                                <p className="mt-3 text-sm text-blue-500">21 October 2019</p>
+                    <div>
+                        {publishedBlogs.length > 0 ? (
+                            <div className="grid grid-cols-1 gap-8 mt-8 lg:grid-cols-2">
+                                {publishedBlogs.map((blog) => (
+                                    <div key={blog._id}>
+                                        <img
+                                            className="relative z-10 object-cover w-full rounded-md h-96"
+                                            src={blog.imageUrl}
+                                            alt={blog.title}
+                                        />
+                                        <div className="relative z-20 max-w-lg p-6 mx-auto -mt-20 bg-white rounded-md shadow">
+                                            <a href="#" className="font-semibold text-gray-800 hover:underline md:text-xl">
+                                                {blog.title}
+                                            </a>
+                                            <p className="mt-3 text-sm text-gray-500 md:text-sm">
+                                                {blog.content}
+                                            </p>
+                                            <p className="mt-3 text-sm text-blue-500">{new Date(blog.date).toLocaleDateString()}</p>
+                                        </div>
+                                    </div>
+                                ))}
                             </div>
-                        </div>
-
-                        <div>
-                            <img className="relative z-10 object-cover w-full rounded-md h-96" src="https://i.ibb.co/MVQrjWB/cef58128383186c0b86de77f53e39e866eae10a2.jpg" alt="" />
-
-                            <div className="relative z-20 max-w-lg p-6 mx-auto -mt-20 bg-white rounded-md shadow">
-                                <a href="#" className="font-semibold text-gray-800 hover:underline md:text-xl">
-                                    How to use sticky note for problem solving
-                                </a>
-
-                                <p className="mt-3 text-sm text-gray-500 md:text-sm">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veritatis sint autem nesciunt,
-                                    laudantium quia tempore delect
-                                </p>
-
-                                <p className="mt-3 text-sm text-blue-500">20 October 2019</p>
+                        ) : (
+                            <div className='flex items-center justify-center min-h-[calc(100vh-300px)]'>
+                                <div className="text-center">
+                                    <h2>No blog available</h2>
+                                    <br />
+                                    <h2>Please add a blog</h2>
+                                </div>
                             </div>
-                        </div>
+                        )}
                     </div>
                 </div>
             </section>
